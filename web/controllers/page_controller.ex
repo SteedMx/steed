ae39c8a81ab3fac5782dc96d971867cf7355e8e4
Zura Guerra <guerrazuramx@gmail.com>
2016-10-04 15:29:42 -0500

    Render pages based on slug

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,6 @@
 defmodule Cheval.PageController do
   use Cheval.Web, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
+  def index(conn, _params), do: render conn, "index.html"
+  def pages(conn, %{"route" => route}), do: render conn, route <> ".html"
 end
