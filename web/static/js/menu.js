05431f1098b20541ce5174cf160edaed5f62b84a
Zura Guerra <guerrazuramx@gmail.com>
2016-10-10 15:17:15 -0500

    Open and close Solutions submenu on click

--- web/static/js/menu.js
+++ web/static/js/menu.js
@@ -3,3 +3,8 @@ $('#menu-trigger').click(function() {
   $(this).toggleClass('menu-open menu-close')
   $('#menu').toggleClass('menu-hidden menu-visible')
 })
+
+// Open and close Solutions submenu
+$('#solutions-menu').click(function() {
+  $('.menu-sub-item').toggleClass('menu-hidden menu-visible')
+})
