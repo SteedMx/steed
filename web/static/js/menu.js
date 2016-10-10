510eb715415b5e5a978aec76eadbe96aa58ee617
Zura Guerra <guerrazuramx@gmail.com>
2016-10-10 01:09:47 -0500

    Rewrite open-close menu handler

--- web/static/js/menu.js
+++ web/static/js/menu.js
@@ -1,11 +1,5 @@
-// Open menu
-$("#menu-trigger-open").click(function() {
-  $(this).switchClass("menu-trigger-open", "menu-trigger-close")
-  $("#menu-hidden").switchClass("menu-hidden", "menu-visible")
-})
-
-// Close menu
-$("#menu-trigger-close").click(function() {
-  $(this).switchClass("menu-trigger-close", "menu-trigger-open")
-  $("#menu-visible").switchClass("menu-visible", "menu-hidden")
+// Open and close menu
+$('#menu-trigger').click(function() {
+  $(this).toggleClass('menu-open menu-close')
+  $('#menu').toggleClass('menu-hidden menu-visible')
 })
