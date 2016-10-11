03ee9bcd27f80b817f39435a8a35812b112819b7
Zura Guerra <guerrazuramx@gmail.com>
2016-10-11 16:55:39 -0500

    Add super awesome parallax effect to landing page :D

--- web/static/js/parallax.js
+++ web/static/js/parallax.js
@@ -1,20 +1,28 @@
-// Text parallax
+// Landing parallax
 (function(){
 
-  var textParallax = document.querySelectorAll(".text-parallax")
-  var speed = 0.009
+  var textParallax1 = document.querySelectorAll(".text-parallax-1")
+  var textParallax2 = document.querySelectorAll(".text-parallax-2")
+  var textParallax3 = document.querySelectorAll(".text-parallax-3")
+  var viewport = 800
 
   window.onscroll = function(){
-    [].slice.call(textParallax).forEach(function(element,i){
-
-      var scrollAmountPerSection = (window.scrollY % 1600)
-      var elementPosition = (12 - (scrollAmountPerSection / 100)) + "em"
-
-      console.log("SCROLL", scrollAmountPerSection)
-      console.log("ELEMENT POSITION", elementPosition)
-
+    [].slice.call(textParallax1).forEach(function(element,i){
+      var scrollAmountPerSection = window.scrollY
+      var elementPosition = 120 - (scrollAmountPerSection / 8) + "vh"
       element.style.bottom = elementPosition
+    })
 
+    ;[].slice.call(textParallax2).forEach(function(element,i){
+      var scrollAmountPerSection = window.scrollY - viewport
+      var elementPosition = 120 - (scrollAmountPerSection / 8) + "vh"
+      element.style.bottom = elementPosition
+    })
+
+    ;[].slice.call(textParallax3).forEach(function(element,i){
+      var scrollAmountPerSection = window.scrollY - (viewport * 2)
+      var elementPosition = 120 - (scrollAmountPerSection / 8) + "vh"
+      element.style.bottom = elementPosition
     })
   }
 
