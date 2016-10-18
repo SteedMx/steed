8fc118ff389c43336fa3c857fb1fb3532a362e08
Zura Guerra <guerrazuramx@gmail.com>
2016-10-18 16:58:44 -0500

    Open and close Be quoted section on click

--- web/static/js/menu.js
+++ web/static/js/menu.js
@@ -2,6 +2,10 @@
 $('#menu-trigger').click(function() {
   $(this).toggleClass('menu-open menu-close')
   $('#menu').toggleClass('menu-hidden menu-visible')
+  if ($('#be-quoted').hasClass('be-quoted-close')) {
+    $('#be-quoted').toggleClass('be-quoted-open be-quoted-close')
+    $('#be-quoted-section').toggleClass('be-quoted-hidden be-quoted-visible')
+  }
 })
 
 // Open and close Solutions submenu
---
+++ web/static/js/requestQuote.js
@@ -0,0 +1,5 @@
+// Open and close menu
+$('#be-quoted').click(function() {
+  $(this).toggleClass('be-quoted-open be-quoted-close')
+  $('#be-quoted-section').toggleClass('be-quoted-hidden be-quoted-visible')
+})
