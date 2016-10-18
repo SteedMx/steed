86b1907f657bfaa3c0995932b9d7926fb622bafa
Zura Guerra <guerrazuramx@gmail.com>
2016-10-18 16:05:14 -0500

    Be quoted section details

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,22 @@
 defmodule Cheval.LayoutView do
   use Cheval.Web, :view
+
+  def services, do: [
+    "Producción de eventos",
+    "Desarrollo de software a la medida",
+    "Soluciones de hardware",
+    "Shared-economy engine",
+    "Diseño web",
+    "Desarrollo móvil",
+    "UI/UX",
+    "Chatbots"
+  ]
+
+  def budgets, do: [
+    "< $30,000",
+    "$30,000 - $50,000",
+    "$50,000 - $150,000",
+    "$150,000 - $300,000",
+    "> $300,000"
+  ]
 end
