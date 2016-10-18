f387377561c3f814b0688d0323d4ce0ce7ec3410
Zura Guerra <guerrazuramx@gmail.com>
2016-10-18 17:59:15 -0500

    Format deadline to string

--- web/controllers/quote_controller.ex
+++ web/controllers/quote_controller.ex
@@ -11,10 +11,14 @@ defmodule Cheval.QuoteController do
   end
 
   defp send_request(request_params) do
+    request_params = Map.put(request_params, "deadline", date_to_string(request_params["deadline"]))
     changeset = Request.changeset(%Request{}, request_params)
     case Repo.insert(changeset) do
       {:ok, request} -> request
       {:error, changeset} -> changeset
     end
   end
+
+  defp date_to_string(%{"year" => year, "month" => month, "day" => day}),
+    do: "#{year}-#{month}-#{day}"
 end
