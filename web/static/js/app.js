34443d0ef60ad80cefca07b2d82637c34750b1ab
Alejandro Cuenca Estrada <alexcuenca4@gmail.com>
2016-10-25 18:14:26 -0500

    Fake landing page

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,25 +1,11 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+// Landing parallax
+(function(){
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
+  $(window).scroll(function(){
+    $("section div").each(function(){
+    $(this).css('margin-top',$(window).scrollTop()-$(this).parent().position().top);
+        console.log();
+    });
+});
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
-import menu from "./menu"
-import scroller from "./scroller"
-import parallax from "./parallax"
-import requestQuote from "./requestQuote"
+})()
--- web/static/js/parallax.js
+++ web/static/js/parallax.js
@@ -1,29 +1,11 @@
 // Landing parallax
 (function(){
 
-  var textParallax1 = document.querySelectorAll(".text-parallax-1")
-  var textParallax2 = document.querySelectorAll(".text-parallax-2")
-  var textParallax3 = document.querySelectorAll(".text-parallax-3")
-  var viewport = 800
-
-  window.onscroll = function(){
-    [].slice.call(textParallax1).forEach(function(element,i){
-      var scrollAmountPerSection = window.scrollY
-      var elementPosition = 120 - (scrollAmountPerSection / 8) + "vh"
-      element.style.bottom = elementPosition
-    })
-
-    ;[].slice.call(textParallax2).forEach(function(element,i){
-      var scrollAmountPerSection = window.scrollY - viewport
-      var elementPosition = 120 - (scrollAmountPerSection / 8) + "vh"
-      element.style.bottom = elementPosition
-    })
-
-    ;[].slice.call(textParallax3).forEach(function(element,i){
-      var scrollAmountPerSection = window.scrollY - (viewport * 2)
-      var elementPosition = 120 - (scrollAmountPerSection / 8) + "vh"
-      element.style.bottom = elementPosition
-    })
-  }
+  $(window).scroll(function(){
+    $("section div").each(function(){
+    $(this).css('margin-top',$(window).scrollTop()-$(this).parent().position().top);
+        console.log();
+    });
+});
 
 })()
